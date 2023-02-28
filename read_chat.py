46fae0def7e811a95635d94fda222007aae43a1a
Samokhin Alexander <icorwin74@gmail.com>
2023-02-28 20:49:06 +0300

    Handle connection errors

--- read_chat.py
+++ read_chat.py
@@ -1,12 +1,29 @@
 import asyncio
+import aiofiles
 
 
-async def monitor_chat():
+async def listen_chat():
     reader, _ = await asyncio.open_connection(
         'minechat.dvmn.org', 5000)
+    async with aiofiles.open('secret_chat.txt', 'a') as f:
+        while not reader.at_eof():
+            data = await reader.readline()
+            data = data.decode()
+            print(data)
+            await f.writelines(data)
 
-    while not reader.at_eof():
-        data = await reader.readline()
-        print(f'{data.decode()}')
 
-asyncio.run(monitor_chat())
+async def tcp_reconnect():
+    while True:
+        print('Connecting to chat...')
+        try:
+            await listen_chat()
+        except ConnectionRefusedError:
+            print('Connection to chat failed!')
+        except asyncio.TimeoutError:
+            print('Connection to chat timed out!')
+        else:
+            print('Connection to chat is closed.')
+
+
+asyncio.run(tcp_reconnect())
