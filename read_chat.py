59fa6ec90587eb6d4840a7c3093f313562440024
Samokhin Alexander <icorwin74@gmail.com>
2023-03-01 16:08:11 +0300

    Add datetime stamp to log

--- read_chat.py
+++ read_chat.py
@@ -1,29 +1,39 @@
+import datetime
 import asyncio
 import aiofiles
+import socket
 
 
-async def listen_chat():
+def get_datetime_now():
+    return datetime.datetime.now().strftime("%d.%m.%y %H:%M:%S")
+
+
+async def capture_chat():
     reader, _ = await asyncio.open_connection(
         'minechat.dvmn.org', 5000)
     async with aiofiles.open('secret_chat.txt', 'a') as f:
         while not reader.at_eof():
-            data = await reader.readline()
-            data = data.decode()
-            print(data)
-            await f.writelines(data)
+            future = reader.readline()
+            data = await asyncio.wait_for(future, 30)
+            formatted_date = get_datetime_now()
+            print(f'[{formatted_date}] {data.decode()}')
+            await f.writelines(f'[{formatted_date}]  {data}')
 
 
 async def tcp_reconnect():
     while True:
-        print('Connecting to chat...')
+        print(f'[{get_datetime_now()}] Connecting to chat...')
         try:
-            await listen_chat()
+            await capture_chat()
         except ConnectionRefusedError:
-            print('Connection to chat failed!')
+            print(f'[{get_datetime_now()}] Connection to chat failed!')
         except asyncio.TimeoutError:
-            print('Connection to chat timed out!')
+            print(f'[{get_datetime_now()}] Connection to chat timed out!')
+        except socket.error as exc:
+            print(f'[{get_datetime_now()}] Caught exception socket.error : {exc}')
         else:
-            print('Connection to chat is closed.')
+            print(f'[{get_datetime_now()}] Connection to chat is closed.')
+        await asyncio.sleep(5)
 
 
 asyncio.run(tcp_reconnect())
