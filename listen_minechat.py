34e26f817d9fa081db24355136f0c6f9c74ae68f
Samokhin Alexander <icorwin74@gmail.com>
2023-03-12 19:31:18 +0300

    Add closing socket on exit

--- listen_minechat.py
+++ listen_minechat.py
@@ -1,6 +1,7 @@
 import datetime
 import socket
 import asyncio
+from contextlib import asynccontextmanager
 import aiofiles
 import dotenv
 import configargparse
@@ -9,6 +10,16 @@ import configargparse
 READING_TIMEOUT = 600
 RECONNECT_DELAY = 30
 HISTORY_FILENAME = 'history.txt'
+
+
+@asynccontextmanager
+async def open_sosket(host, port):
+    reader, writer = await asyncio.open_connection(host, port)
+    try:
+        yield reader
+    finally:
+        writer.close()
+        await writer.wait_closed()
 
 
 def get_datetime_now():
@@ -20,18 +31,15 @@ async def capture_chat(options):
         history_filename = options.history
     else:
         history_filename = HISTORY_FILENAME
-    reader, _ = await asyncio.open_connection(
-        options.host,
-        options.port_out,
-    )
-    async with aiofiles.open(history_filename, 'a') as f:
-        while not reader.at_eof():
-            future = reader.readline()
-            line = await asyncio.wait_for(future, READING_TIMEOUT)
-            formatted_date = get_datetime_now()
-            decoded_line = line.decode()
-            print(f'[{formatted_date}] {decoded_line}', end='')
-            await f.write(f'[{formatted_date}] {decoded_line}')
+    async with open_sosket(options.host, options.port_out) as reader:
+        async with aiofiles.open(history_filename, 'a') as f:
+            while not reader.at_eof():
+                future = reader.readline()
+                line = await asyncio.wait_for(future, READING_TIMEOUT)
+                formatted_date = get_datetime_now()
+                decoded_line = line.decode()
+                print(f'[{formatted_date}] {decoded_line}', end='')
+                await f.write(f'[{formatted_date}] {decoded_line}')
 
 
 async def reconnect(options):
